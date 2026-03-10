0c304aefad0312c748265ce17c24c487ebcbf25e
Jean Pedro <jjeanpedro20@gmail.com>
2026-03-10 19:55:28 -0300

    Classe_Conta

---
+++ Menu.ts
@@ -0,0 +1,105 @@
+import readlinesync = require("readline-sync");
+import { colors } from "./src/util/Colors";
+import { Conta } from "./src/model/Conta";
+
+export function main() {
+    let opcao: number;
+
+    const conta: Conta = new Conta(1, 123, 1, "Adriana", 10000);
+    conta.visualizar();
+    conta.sacar(10500);
+    conta.visualizar();
+    conta.depositar(5000);
+    conta.visualizar();
+
+    while (true) {
+        const amarelo = colors.bg.black + colors.fg.yellow;
+        const reset = colors.reset;
+
+        console.log(amarelo + "*****************************************************" + reset);
+        console.log(amarelo + "                                                     " + reset);
+        console.log(amarelo + "                BANCO DO BRAZIL COM Z                " + reset);
+        console.log(amarelo + "                                                     " + reset);
+        console.log(amarelo + "*****************************************************" + reset);
+        console.log(amarelo + "                                                     " + reset);
+        console.log(amarelo + "            1 - Criar Conta                          " + reset);
+        console.log(amarelo + "            2 - Listar todas as Contas               " + reset);
+        console.log(amarelo + "            3 - Buscar Conta por Numero              " + reset);
+        console.log(amarelo + "            4 - Atualizar Dados da Conta             " + reset);
+        console.log(amarelo + "            5 - Apagar Conta                         " + reset);
+        console.log(amarelo + "            6 - Sacar                                " + reset);
+        console.log(amarelo + "            7 - Depositar                            " + reset);
+        console.log(amarelo + "            8 - Transferir valores entre Contas      " + reset);
+        console.log(amarelo + "            9 - Sair                                 " + reset);
+        console.log(amarelo + "                                                     " + reset);
+        console.log(amarelo + "*****************************************************" + reset);
+
+        console.log("\nEntre com a opção desejada: ");
+        opcao = readlinesync.questionInt("");
+
+        if (opcao == 9) {
+            console.log(
+                colors.fg.greenstrong,
+                "\nBanco do Brazil com Z - O seu Futuro começa aqui!",
+            );
+            sobre();
+            console.log(colors.reset, "");
+            process.exit(0);
+        }
+
+        switch (opcao) {
+            case 1:
+                console.log(colors.fg.whitestrong + "\n\nCriar Conta\n\n" + colors.reset);
+                keyPress();
+                break;
+            case 2:
+                console.log(colors.fg.whitestrong + "\n\nListar todas as Contas\n\n" + colors.reset);
+                keyPress();
+                break;
+            case 3:
+                console.log(colors.fg.whitestrong + "\n\nConsultar dados da Conta - por número\n\n" + colors.reset);
+                keyPress();
+                break;
+            case 4:
+                console.log(colors.fg.whitestrong + "\n\nAtualizar dados da Conta\n\n" + colors.reset);
+                keyPress();
+                break;
+            case 5:
+                console.log(colors.fg.whitestrong + "\n\nApagar uma Conta\n\n" + colors.reset);
+                keyPress();
+                break;
+            case 6:
+                console.log(colors.fg.whitestrong + "\n\nSaque\n\n" + colors.reset);
+                keyPress();
+                break;
+            case 7:
+                console.log(colors.fg.whitestrong + "\n\nDepósito\n\n" + colors.reset);
+                keyPress();
+                break;
+            case 8:
+                console.log(colors.fg.whitestrong + "\n\nTransferência entre Contas\n\n" + colors.reset);
+                keyPress();
+                break;
+            default:
+                console.log(colors.fg.redstrong + "\nOpção Inválida!\n" + colors.reset);
+                keyPress();
+                break;
+        }
+    }
+}
+
+function sobre(): void {
+    console.log("\n*****************************************************");
+    console.log("Projeto Desenvolvido por: ");
+    console.log("Jean Pedro - jjeanpedro20@gmail.com");
+    console.log("https://github.com/jjeanpedro03");
+    console.log("*****************************************************");
+}
+
+function keyPress(): void {
+    console.log(colors.reset, "");
+    console.log("\nPressione enter para continuar...");
+    readlinesync.prompt();
+}
+
+main();
